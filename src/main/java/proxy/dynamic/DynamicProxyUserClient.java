e97c20a8fa70fb2cb4ab18e89360191145a52d90
ClearWang <1585993500@qq.com>
2021-06-13 15:37:04 +0800

    java中3中代理模式总结

---
+++ src/main/java/proxy/dynamic/DynamicProxyUserClient.java
@@ -0,0 +1,18 @@
+package proxy.dynamic;
+
+import java.util.HashMap;
+import java.util.Map;
+import service.OrderService;
+
+/**
+ * dynamic proxy
+ *
+ * @author clewill
+ * @create 2021:05:12 16:33
+ **/
+public class DynamicProxyUserClient {
+    public static void main(String[] args){
+        Map<String, String> specifiers = new HashMap<>();
+        System.out.println(specifiers.size());
+    }
+}
---
+++ src/main/java/proxy/staticproxy/ProxyOrderClient.java
@@ -0,0 +1,29 @@
+package proxy.staticproxy;
+
+import service.OrderService;
+import service.impl.OutSideOrderServiceImpl;
+
+/**
+ * 订单代理类
+ * @author clewill
+ * @create 2021:05:11 14:18
+ **/
+public class ProxyOrderClient implements OrderService {
+
+  /**
+   * 真正的订单服务
+   */
+  private final OrderService orderService;
+
+  public ProxyOrderClient(){
+    this.orderService = new OutSideOrderServiceImpl();
+  }
+
+  public ProxyOrderClient(OrderService orderService){
+    this.orderService = orderService;
+  }
+  @Override
+  public void saveOrders() {
+    orderService.saveOrders();
+  }
+}
